7676cac818bb808cd74feeae4a2f6c547ba2dc1e
jkorda20 <jkorda2011@gmail.com>
2025-11-01 14:52:28 +0000

    added things

--- scripts/dogApi.js
+++ scripts/dogApi.js
@@ -8,10 +8,13 @@ function apiApp() {
       try {
         // STEP 1: Look at https://dog.ceo/api/breeds/image/random/3
         // STEP 2: Add URL below
+        const Url = "https://dog.ceo/api/breeds/image/random/3"
+        const res = await axios.get(Url)
         // const res = await axios.get(Url);
         // STEP 3: console.log(res.data)
         // STEP 4: 
         this.render(res.data.message);
+
       } catch (err) {
         out.innerHTML = `<p class='text-red-600'>Something went wrong 😢</p>`;
         console.error(err);
