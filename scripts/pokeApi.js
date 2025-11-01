d6cc244f33718bbdcbce3c5c416dbf2a7b8a7357
jkorda20 <jkorda2011@gmail.com>
2025-11-01 15:07:54 +0000

    changes

--- scripts/pokeApi.js
+++ scripts/pokeApi.js
@@ -8,10 +8,10 @@ function apiApp() {
       try {
         // STEP 1: Visit https://pokeapi.co/api/v2/pokemon?limit=3
         // STEP 2: Add endpoint
-        // const res = await axios.get("YOUR_URL_HERE");
+        const res = await axios.get("YOUR_URL_HERE");
         // STEP 3: Identify res.data.results
-        // STEP 4: const details = await Promise.all(...)
-        // STEP 5: this.render(details);
+        const details = await Promise.all(res.data.results)
+        this.render(details);
       } catch (err) {
         out.innerHTML = `<p class='text-red-600'>Error loading Pokémon ⚠️</p>`;
         console.error(err);
